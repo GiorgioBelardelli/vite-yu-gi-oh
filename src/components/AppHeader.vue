df779f431d42c96b6c850dec140d71346a341b9e
GiorgioBelardelli <asterapocalypse@gmail.com>
2024-01-08 15:55:32 +0100

    Setup base Frame + AppHeader

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+    name: 'AppHeader',
+    props: ['message'],
+}
+</script>
+
+<template>
+    <header>
+        <div class="container">
+            <h1>{{ message }}</h1>
+        </div>
+    </header>
+</template>
+
+<style lang="scss" scoped>
+@use '../styles/general.scss' as *;
+@use '../styles/partials/variables.scss' as *;
+
+header {
+    background-color: $white;
+    padding: 1rem;
+}
+</style>
---
+++ src/components/CardList.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+    name: 'CardList',
+}
+</script>
+
+<template>
+    <div class="container">
+        <h1>FILTRO VA QUA</h1>
+    </div>
+    <section>
+        <div class="container">
+            <h1>CARTE</h1>
+        </div>
+    </section>
+</template>
+
+<style lang="scss" scoped>
+@use '../styles/general.scss' as *;
+@use '../styles/partials/variables.scss' as *;
+</style>
